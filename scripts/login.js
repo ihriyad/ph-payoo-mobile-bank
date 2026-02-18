7dbc10ffcf50d38d25dc5894c5f3c91991aba575
ihriyad <ihriyad.dev@gmail.com>
2026-02-18 19:38:18 +0600

    fixed alert

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,18 @@
+function showAlert(message, type) {
+  const alertBox = document.getElementById("alertBox");
+
+  alertBox.innerHTML = `
+    <div class="alert alert-${type} mb-4">
+      <span>${message}</span>
+    </div>
+  `;
+
+  setTimeout(() => {
+    alertBox.innerHTML = "";
+  }, 3000);
+}
+
+
 document.getElementById("btn-login").addEventListener("click", function () {
   const inputNumber = document.getElementById("input-number");
   const number = inputNumber.value;
@@ -5,27 +20,15 @@ document.getElementById("btn-login").addEventListener("click", function () {
   const inputPin = document.getElementById("input-pin");
   const pin = inputPin.value;
 
-  const successAlert = document.getElementById("alert-box");
-  const errorAlert = document.getElementById("alert-box2");
-
-  successAlert.classList.add("hidden");
-  errorAlert.classList.add("hidden");
-
-  if (number === "01300000000" && pin === "4321") {
-    successAlert.classList.remove("hidden");
-    window.location.assign("./home.html");
-  } else {
-    errorAlert.classList.remove("hidden");
+  if (number === "0134567899" && pin === "1234") {
+    window.location.assign("/home.html");
+  } else if (number === "" || pin === "") {
+    showAlert("Please fill all fields!", "warning");
+  } else if (pin.length < 4) {
+    showAlert("Pin must be at least 4 characters!", "warning");
+  } else if (number !== "0134567899" || pin !== "1234") {
+    showAlert("Invalid Number or Pin", "error");
   }
+
   return;
 });
-const inputs = document.getElementsByClassName("input");
-for (const input of inputs) {
-  input.addEventListener("click", function () {
-    const successAlert = document.getElementById("alert-box");
-    const errorAlert = document.getElementById("alert-box2");
-
-    successAlert.classList.add("hidden");
-    errorAlert.classList.add("hidden");
-  });
-}
